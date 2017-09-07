7c50bd28ab83228c75521dbfc5cbbb6abcbea085
sakiya <joyconan@qq.com>
2017-09-07 11:36:27 +0900

    9.07fix

--- src/main/java/com/neusoft/repository/DeptRepository.java
+++ src/main/java/com/neusoft/repository/DeptRepository.java
@@ -2,6 +2,8 @@ package com.neusoft.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.neusoft.domain.Dept;
 
